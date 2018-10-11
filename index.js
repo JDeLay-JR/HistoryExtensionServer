710f069f8952cdd0514b71269e98d165f849917f
JDeLay-JR <jdelay.jr@gmail.com>
2018-10-10 20:27:11 -0400

    feat: initialized server

---
+++ index.js
@@ -0,0 +1,60 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const compression = require('compression')
+const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 8080
+const app = express()
+module.exports = app
+
+const createApp = () => {
+  // logging middleware
+  app.use(morgan('dev'))
+
+  // body parsing middleware
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }))
+
+  // compression middleware
+  app.use(compression())
+
+  // session middleware with passport
+  app.use('/auth', require('./auth'))
+  
+  // static file-serving middleware
+  app.use(express.static(path.join(__dirname, '..', 'public')))
+
+  // any remaining requests with an extension (.js, .css, etc.) send 404
+  app.use((req, res, next) => {
+    if (path.extname(req.path).length) {
+      const err = new Error('Not found')
+      err.status = 404
+      next(err)
+    } else {
+      next()
+    }
+  })
+
+  // error handling endware
+  app.use((err, req, res, next) => {
+    console.error(err)
+    console.error(err.stack)
+    res.status(err.status || 500).send(err.message || 'Internal server error.')
+  })
+}
+
+const startListening = () => {
+  // start listening (and create a 'server' object representing our server)
+  const server = app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
+}
+
+// This evaluates as true when this file is run directly from the command line,
+// i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
+// It will evaluate false when this module is required by another module - for example,
+// if we wanted to require our app in a test spec
+if (require.main === module) {
+  createApp()
+    .then(startListening)
+} else {
+  createApp()
+}
